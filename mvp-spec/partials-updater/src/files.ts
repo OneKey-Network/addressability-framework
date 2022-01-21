ff9779340f938ca583f400d7f0795953267cffd3
Romain Lofaso <r.lofaso@criteo.com>
2022-01-21 15:38:18 +0100

    Use camel case with variable

--- mvp-spec/partials-updater/src/files.ts
+++ mvp-spec/partials-updater/src/files.ts
@@ -2,34 +2,34 @@ import RootPath from "app-root-path";
 import fs from "fs";
 import path from "path";
 
-const BinDir = path.join(RootPath.path, "node_modules", ".bin");
-const AssetsDir = path.join(RootPath.path, "..", "assets");
-const AssetPathForDocuments = path.join(".", "assets");
-const PartialsDir = path.join(RootPath.path, "..", "partials");
-const DocumentsDir = path.join(RootPath.path, "..");
-const DocumentExts = [ ".md" ]
+const binDir = path.join(RootPath.path, "node_modules", ".bin");
+const assetsDir = path.join(RootPath.path, "..", "assets");
+const assetPathForDocuments = path.join(".", "assets");
+const partialsDir = path.join(RootPath.path, "..", "partials");
+const documentsDir = path.join(RootPath.path, "..");
+const documentExts = [ ".md" ]
 
 
-export const getBinPath = (bin: string): string => path.join(BinDir, bin);
+export const getBinPath = (bin: string): string => path.join(binDir, bin);
 
-export const getAssetPath = (file: string): string => path.join(AssetsDir, file);
+export const getAssetPath = (file: string): string => path.join(assetsDir, file);
 
-export const getAssetPathRelativeToDocuments = (file: string): string => path.join(AssetPathForDocuments, file);
+export const getAssetPathRelativeToDocuments = (file: string): string => path.join(assetPathForDocuments, file);
 
-export const getPartialPath = (file: string): string => path.join(PartialsDir, file);
+export const getPartialPath = (file: string): string => path.join(partialsDir, file);
 
 /** Load a partial based on its name. */
-export const loadPartial = (file: string): Promise<string> => loadFile(file, PartialsDir);
+export const loadPartial = (file: string): Promise<string> => loadFile(file, partialsDir);
 
 /** Load a document based on its name. */
-export const loadDocument = (file: string): Promise<string> => loadFile(file, DocumentsDir);
+export const loadDocument = (file: string): Promise<string> => loadFile(file, documentsDir);
 
 /** List the documents by names. */
-export const listDocuments = (): Promise<string[]> => listFiles(DocumentsDir, isDocument);
+export const listDocuments = (): Promise<string[]> => listFiles(documentsDir, isDocument);
 
 /** Rewrite a document. */
 export function rewriteDocument(name: string, content: string): Promise<void> {
-    const fullPath = path.join(DocumentsDir, name);
+    const fullPath = path.join(documentsDir, name);
     if (!fs.existsSync(fullPath)) {
         throw new Error(`File doesn't exist. Cannot rewrite: ${fullPath}`)
     }
@@ -46,7 +46,7 @@ async function loadFile(name: string, dir: string): Promise<string> {
     return contentStr;
 }
 
-const isDocument = (file :string): boolean => DocumentExts.some(e => file.endsWith(e));
+const isDocument = (file :string): boolean => documentExts.some(e => file.endsWith(e));
 
 async function listFiles(dir: string, filter: (n: string) => boolean): Promise<string[]> {
     const files = await fs.promises.readdir(dir);
