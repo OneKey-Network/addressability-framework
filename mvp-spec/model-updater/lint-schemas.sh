d6a1fd838721129b23b556c71384381a750eeb39
Romain Lofaso <r.lofaso@criteo.com>
2022-03-17 15:09:44 +0100

    Move the transmissions schemas to root schema directory

--- mvp-spec/model-updater/lint-schemas.sh
+++ mvp-spec/model-updater/lint-schemas.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-SCHEMAS_DIR=../json-schemas/transmission 
+SCHEMAS_DIR=../json-schemas 
 
 find ${SCHEMAS_DIR}  -name '*.json' \
     -exec npx jsonlint --in-place --pretty-print -indent "    " {} \;
